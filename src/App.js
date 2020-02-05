d2fb045f4e302624a99c770984f8a29b058955cf
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-02-05 17:58:37 -0600

    Starting Project CRM-Clients

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Header from './components/layout/Header'
+import Nav from './components/layout/Nav'
+import Clients from './components/clients/Clients'
+import Products from './components/products/Products'
+import Orders from './components/orders/Orders'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Header/>
+        <div className="grid contenedor contenido-principal">
+          <Nav/>
+          <main className="caja-contenido col-9">
+            {/* // TODO: Routing a los diferentes componentes */}
+            <Switch>
+              <Route exact path="/" component={Clients}></Route>
+              <Route exact path="/productos" component={Products}></Route>
+              <Route exact path="/pedidos" component={Orders}></Route>
+            </Switch>
+          </main>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
